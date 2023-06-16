a1da8bce37bce758cc523a381d811766fbf5aa5b
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-06-16 16:44:00 -0300

    Adiciona função que verifica se um número é primo.

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 // import * as ms from "../../meusScripts.js"
 
-// criança (0-12 anos), adolescente (13-17 anos), adulto (18-59 anos) ou idoso (60 anos ou mais)
-
 function print(a) {
     console.log(a)
 }
 
-function traduzCor(cor = 0) {
-    let corLower = cor.toLowerCase()
-    switch (corLower) {
-        case 0:
-            print("Cor não inserida")
-            return 0
-        case "red":
-            print("A cor " + corLower + " significa: Vermelho.")
-            return 1
-        case "blue":
-            print("A cor " + corLower + " significa: Azul.")
-            return 1
-        case "green":
-            print("A cor " + corLower + " significa: Verde.")
-            return 1
-        case "yellow":
-            print("A cor " + corLower + " significa: Amarelo.")
-            return 1
-        case "grey":
-            print("A cor " + corLower + " significa: Cinza.")
-            return 1
-        default:
-            print("Cor não encontrada.")
-            return -1
+function numeroEPrimo(numero = 0) {
+    if (numero < 0) {
+        print("Digite um número válido!")
+    } else if (numero === 0) {
+        print("Digite um número!")
+    } else {
+        let contador = 0
+        let i = 0
+        do {
+            if (numero % i === 0) {
+                contador++
+                if (contador > 2) {
+                    print(`O número ${numero} não é primo`)
+                    return -1
+                }
+            }
+            i++
+        } while (i <= numero)
+        print(`O número ${numero} é primo`)
+        return 1
     }
 }
 
-traduzCor("Yellow")
+// for (let i = 0; i <= 25; i++) {
+//     numeroEPrimo(i)
+// }
+
+numeroEPrimo(5)
