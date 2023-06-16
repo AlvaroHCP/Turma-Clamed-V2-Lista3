8145b0c58b815cde6f4a261b54aff2300312325e
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-06-16 17:22:30 -0300

    Adiciona função que identifica palavras com mais de 5 letras.

--- script.js
+++ script.js
@@ -4,30 +4,19 @@ function print(a) {
     console.log(a)
 }
 
-function Fibonacci(numero = 0) {
-    if (numero < 0) {
-        print("Digite um número válido!")
+function palavrasComMaisDeXLetras(array = []) {
+    let letras = 5
+    if (array === []) {
+        print("Insira uma lista de palavras!")
     } else {
-        let fibonacci = []
-        let soma = 0
-        let i = 0
-        print(`\nA sequência até o número ${numero} é: `)
-        while (soma <= numero) {
-            fibonacci.push(soma)
-
-            if (i === 0) {
-                soma += 1
-            } else {
-                soma += fibonacci[i - 1]
+        let listaResultado = []
+        for (const palavra of array) {
+            if (palavra.length >= letras) {
+                listaResultado.push(palavra)
             }
-            i++
         }
-        print(fibonacci.join(", ") + "\n")
+        print(`\nAs palavras com mais de ${letras} letras são: ${listaResultado.join(", ")}\n`)
     }
 }
 
-// for (let i = 0; i <= 21; i = i + 3) {
-//     Fibonacci(i)
-// }
-
-Fibonacci(10)
+palavrasComMaisDeXLetras(["Alvaro", "Aline", "Soda", "Marta", "Mar"])
