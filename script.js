6b75093ad163ee7fe81f0bcaac70cfe0f56691c8
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-06-16 15:12:34 -0300

    Adiciona função que traduz a cor digitada para o português.

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
+// import * as ms from "../../meusScripts.js"
+
 // criança (0-12 anos), adolescente (13-17 anos), adulto (18-59 anos) ou idoso (60 anos ou mais)
 
 function print(a) {
     console.log(a)
 }
 
-function classificaIdade(idade = 0) {
-
-    if (idade < 0) {
-        print("Idade = " + idade + ". Idade incorreta!")
-    } else if (idade <= 12) {
-        print("Idade = " + idade + ". É criança.")
-    } else if (idade <= 17) {
-        print("Idade = " + idade + ". É adolescente.")
-    } else if (idade <= 59) {
-        print("Idade = " + idade + ". É adulto.")
-    } else if (idade <= 125) {
-        print("Idade = " + idade + ". É idoso.")
-    } else if (idade > 125) {
-        print("Idade = " + idade + ". É um Matusalém eterno.")
+function traduzCor(cor = 0) {
+    let corLower = cor.toLowerCase()
+    switch (corLower) {
+        case 0:
+            print("Cor não inserida")
+            return 0
+        case "red":
+            print("A cor " + corLower + " significa: Vermelho.")
+            return 1
+        case "blue":
+            print("A cor " + corLower + " significa: Azul.")
+            return 1
+        case "green":
+            print("A cor " + corLower + " significa: Verde.")
+            return 1
+        case "yellow":
+            print("A cor " + corLower + " significa: Amarelo.")
+            return 1
+        case "grey":
+            print("A cor " + corLower + " significa: Cinza.")
+            return 1
+        default:
+            print("Cor não encontrada.")
+            return -1
     }
 }
 
-// for (i = -1; i <= 130; i++) {
-//     classificaIdade(print, i)
-// }
-
-classificaIdade(15)
+traduzCor("Yellow")
