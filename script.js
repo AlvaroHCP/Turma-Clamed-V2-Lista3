956b242d34e1a44642ecabe1c7986737ca34b997
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-06-16 21:05:19 -0300

    Adiciona função que imprime nome e preço dos objetos(produtos), além do número de produtos e o preço total.

--- script.js
+++ script.js
@@ -4,18 +4,42 @@ function print(a) {
     console.log(a)
 }
 
-function palavrasComMaisDeXLetras(array = [], letras = 5) {
-    if (array === []) {
-        print("Insira uma lista de palavras!")
-    } else {
-        let listaResultado = []
-        for (const palavra of array) {
-            if (palavra.length >= letras) {
-                listaResultado.push(palavra)
-            }
-        }
-        print(`\nAs palavras com mais de ${letras} letras são: ${listaResultado.join(", ")}\n`)
+let dados = [{
+    nome: "Coca-Cola",
+    preco: 7.20
+},
+{
+    nome: "Guarana Antartica",
+    preco: 6.59
+},
+{
+    nome: "farinha de trigo",
+    preco: 4.30
+},
+{
+    nome: "fubá",
+    preco: 5.50
+}]
+
+function nomeEPreco(array) {
+    let i = 0
+    let precoTotal = 0
+    for (const elemento of dados) {
+        precoTotal += Number(elemento.preco)
+        print(`O produto ${Number(i) + 1} é \"${elemento.nome}\" e custa R$${String(Number(elemento.preco).toFixed(2)).replace(".", ",")}`)
+        i++
     }
+    print(`\n\nNo total são ${dados.length} produtos cadastrados, e o preço total é R$${String((precoTotal).toFixed(2)).replace(".", ",")}\n`)
 }
 
-palavrasComMaisDeXLetras(["Alvaro", "Aline", "Soda", "Marta", "Mar"])
+// function nomeEPreco(array) {
+//     let precoTotal = 0
+//     for (const elemento in dados) {
+//         precoTotal += Number(dados[elemento].preco)
+//         print(`O produto ${Number(elemento) + 1} é \"${dados[elemento].nome}\" e custa R$${String((dados[elemento].preco).toFixed(2)).replace(".", ",")}`)
+//     }
+//     print(`\n\nNo total são ${dados.length} produtos cadastrados, e o preço total é R$${String((precoTotal).toFixed(2)).replace(".", ",")}\n`)
+// }
+
+nomeEPreco(dados)
+
